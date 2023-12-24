aa50d28bab76fddf2ec0f5fa0b4d34440e8bbc0d
Ulia <pochta@mail.ru>
2023-12-24 18:34:17 +0300

    commit#2

--- main.cpp
+++ main.cpp
@@ -1,5 +1,11 @@
 #include "sdt.h"
+#include <iostream>
 
-int main() {    
+int main() {
+    int num1, num2;  
+    std::cout << "Введите первое целое число: ";
+    std::cin >> num1; 
+    std::cout << "Введите второе целое число: ";
+    std::cin >> num2; 
     return 0;
 }
