87a6d23d593ba1e80d8852ddd811560ab5312823
Ulia <pochta@mail.ru>
2023-12-24 18:47:58 +0300

    commit#5.B

--- main.cpp
+++ main.cpp
@@ -9,5 +9,7 @@ int main() {
     std::cin >> num2; 
     int sum = num1 + num2;
     std::cout << "Сумма введенных чисел: " << sum << std::endl;
+    int diff = num1 - num2;
+    std::cout << "Разность введенных чисел: " << diff << std::endl;
     return 0;
 }
