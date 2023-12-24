9e52cd40aef6efaf8cd2ee8c38f88a0d4cef4072
Ulia <pochta@mail.ru>
2023-12-24 21:53:45 +0300

    commit#5.A.div

--- main.cpp
+++ main.cpp
@@ -11,6 +11,11 @@ int main() {
     std::cout << "Сумма введенных чисел: " << sum << std::endl;
     int diff = num1 - num2;
     std::cout << "Разность введенных чисел: " << diff << std::endl;
-
+if (num2 != 0) {
+        double quotient = static_cast<double>(num1) / num2; // Приведение к типу double для точности
+        std::cout << "Частное введенных чисел: " << quotient << std::endl;
+    } else {
+        std::cout << "Невозможно выполнить деление на ноль." << std::endl;
+    }
     return 0;
 }
